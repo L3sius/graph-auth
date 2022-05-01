1d9228eaedcb73b194392acb8377187ba1da8be5
Justas <justaslescinskas@gmail.com>
2022-05-02 00:08:26 +0300

    Adding completed landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    LandingPageComponent,
+    RegisterPageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    AppRoutingModule,
   ],
   exports: [MatButtonModule, BrowserAnimationsModule],
   providers: [],
