a5e3dc9db3ff8794fef3422062e479f75426212e
Justas <justaslescinskas@gmail.com>
2022-05-08 14:29:07 +0300

    adding password update

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -15,6 +15,9 @@ interface Category {
 })
 export class LoginPageComponent {
   loginForm: FormGroup;
+
+  selectedType = '';
+  password = new Array();
   //TODO: Add authentication
   //private token: string;
   categories: Category[] = [
@@ -36,6 +39,32 @@ export class LoginPageComponent {
       ]),
       category: new FormControl(null, Validators.required),
     });
+  }
+
+  onSelectChange(event: any) {
+    this.selectedType = event.value;
+  }
+
+  onCheckboxChange(event: any) {
+    console.log(event);
+    console.log(event.srcElement.id);
+    console.log(event.target.checked);
+
+    this.updatePasswordArray(event.srcElement.id, event.target.checked);
+  }
+
+  updatePasswordArray(element: number, isChecked: boolean) {
+    console.log(element);
+    if (isChecked) {
+      this.password.push(element);
+      console.log(this.password);
+    } else {
+      const index = this.password.indexOf(element, 0);
+      if (index > -1) {
+        this.password.splice(index, 1);
+        console.log(this.password);
+      }
+    }
   }
 
   onSubmit(user: User) {
